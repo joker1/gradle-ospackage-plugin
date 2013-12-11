2e44c0925972af01dd25872a47ca3f16539dc563
Christo Zietsman <christo.zietsman@gmail.com>
2013-12-11 17:20:59 +0200

    - Allow setting arch, maintainer, uploaders & priority for Dev packages - Allow specifying configurationFiles (conffiles)

--- src/main/groovy/com/netflix/gradle/plugins/packaging/SystemPackagingTask.groovy
+++ src/main/groovy/com/netflix/gradle/plugins/packaging/SystemPackagingTask.groovy
@@ -59,6 +59,8 @@ public abstract class SystemPackagingTask extends AbstractArchiveTask {
         mapping.map('release', { parentExten?.getRelease()?:getClassifier() })
         mapping.map('version', { parentExten?.getVersion()?:project.getVersion().toString() })
         mapping.map('user', { parentExten?.getUser()?:getPackager() })
+        mapping.map('maintainer', { parentExten?.getMaintainer()?:getPackager() })
+        mapping.map('uploaders', { parentExten?.getUploaders()?:getPackager() })
         mapping.map('permissionGroup', { parentExten?.getPermissionGroup()?:'' })
         mapping.map('packageGroup', { parentExten?.getPackageGroup() })
         mapping.map('buildHost', { parentExten?.getBuildHost()?:getLocalHostName() })
@@ -72,6 +74,7 @@ public abstract class SystemPackagingTask extends AbstractArchiveTask {
         mapping.map('sourcePackage', { parentExten?.getSourcePackage()?:'' })
         mapping.map('provides', { parentExten?.getProvides()?:getPackageName() })
         mapping.map('createDirectoryEntry', { parentExten?.getCreateDirectoryEntry()?:false })
+        mapping.map('priority', { parentExten?.getPriority()?:'optional' })
 
         // Task Specific
         mapping.map('archiveName', { assembleArchiveName() })
@@ -93,6 +96,10 @@ public abstract class SystemPackagingTask extends AbstractArchiveTask {
         use(CopySpecEnhancement) {
             super.copy()
         }
+    }
+
+    def getAllConfigurationFiles() {
+        return getConfigurationFiles() + (parentExten?.getConfigurationFiles()?: [])
     }
 
     def getAllPreInstallCommands() {
